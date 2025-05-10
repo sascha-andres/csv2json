e894b6dfd1c8610883c5210a0a8ec142c4f5f2d6
Sascha Andres <sascha.andres@outlook.com>
2025-05-10 09:12:47 +0200

    fix(#3): support setting multiple values in sub object

--- mapper.go
+++ mapper.go
@@ -188,18 +188,23 @@ func (m *Mapper) Map() error {
 
 // setValue creates and maps nested dictionaries based on a hierarchy of keys, assigning a final value.
 func setValue(hierarchy []string, value any, data map[string]interface{}) map[string]interface{} {
-	v := setValueInternal(hierarchy, value)
+	v := setValueInternal(hierarchy, value, data)
 	data[hierarchy[0]] = v
 	return data
 }
 
 // setValueInternal recursively creates and maps nested dictionaries based on a hierarchy of keys, assigning a final value.
-func setValueInternal(hierarchy []string, value any) any {
+func setValueInternal(hierarchy []string, value any, inside map[string]any) any {
 	if len(hierarchy) == 1 {
 		return value
 	}
-	v := make(map[string]interface{})
-	v[hierarchy[1]] = setValueInternal(hierarchy[1:], value)
+	v := make(map[string]any)
+	if val, ok := inside[hierarchy[0]]; ok {
+		if reflected, ok := val.(map[string]any); ok {
+			v = reflected
+		}
+	}
+	v[hierarchy[1]] = setValueInternal(hierarchy[1:], value, v)
 	return v
 }
 
