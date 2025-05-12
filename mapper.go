be34ae313944ffaafc4758d66db0d5eee4295f4c
Sascha Andres <sascha.andres@outlook.com>
2025-05-12 06:35:20 +0200

    refactor(#11): move helper funcs to own file

--- mapper.go
+++ mapper.go
@@ -275,54 +275,6 @@ func (m *Mapper) getApplicationValue(field CalculatedField, i int) (any, error) 
 	return nil, errors.New("unknown format " + field.Format)
 }
 
-// convertToType converts the input string `val` to a specified type `t` such as "int", "float", or "bool".
-// Returns the converted value as `any` or an error if the conversion fails.
-func convertToType(t, val string) (any, error) {
-	switch t {
-	case "int":
-		i, err := strconv.Atoi(val)
-		if err != nil {
-			return nil, err
-		}
-		return i, nil
-	case "float":
-		f, err := strconv.ParseFloat(val, 64)
-		if err != nil {
-			return nil, err
-		}
-		return f, nil
-	case "bool":
-		b, err := strconv.ParseBool(val)
-		if err != nil {
-			return nil, err
-		}
-		return b, nil
-	}
-	return val, nil
-}
-
-// setValue creates and maps nested dictionaries based on a hierarchy of keys, assigning a final value.
-func setValue(hierarchy []string, value any, data map[string]interface{}) map[string]interface{} {
-	v := setValueInternal(hierarchy, value, data)
-	data[hierarchy[0]] = v
-	return data
-}
-
-// setValueInternal recursively creates and maps nested dictionaries based on a hierarchy of keys, assigning a final value.
-func setValueInternal(hierarchy []string, value any, inside map[string]any) any {
-	if len(hierarchy) == 1 {
-		return value
-	}
-	v := make(map[string]any)
-	if val, ok := inside[hierarchy[0]]; ok {
-		if reflected, ok := val.(map[string]any); ok {
-			v = reflected
-		}
-	}
-	v[hierarchy[1]] = setValueInternal(hierarchy[1:], value, v)
-	return v
-}
-
 // initialize initializes the Mapper instance by reading the mapping file and opening the input and output files.
 func (m *Mapper) initialize() (io.ReadCloser, io.WriteCloser, error) {
 	mappingFile := "mapping.json"
--- mapper_test.go
+++ mapper_test.go
@@ -81,122 +81,6 @@ func TestCreate(t *testing.T) {
 	}
 }
 
-// TestSetValue tests the setValue function which creates and maps nested dictionaries based on a hierarchy of keys.
-func TestSetValue(t *testing.T) {
-	tests := []struct {
-		name      string
-		hierarchy []string
-		value     any
-		data      map[string]interface{}
-		want      map[string]interface{}
-	}{
-		{
-			name:      "simple key",
-			hierarchy: []string{"key"},
-			value:     "value",
-			data:      map[string]interface{}{},
-			want:      map[string]interface{}{"key": "value"},
-		},
-		{
-			name:      "nested key",
-			hierarchy: []string{"parent", "child"},
-			value:     "value",
-			data:      map[string]interface{}{},
-			want:      map[string]interface{}{"parent": map[string]interface{}{"child": "value"}},
-		},
-		{
-			name:      "deeply nested key",
-			hierarchy: []string{"level1", "level2", "level3"},
-			value:     "value",
-			data:      map[string]interface{}{},
-			want:      map[string]interface{}{"level1": map[string]interface{}{"level2": map[string]interface{}{"level3": "value"}}},
-		},
-		{
-			name:      "existing data",
-			hierarchy: []string{"parent", "child2"},
-			value:     "value2",
-			data: map[string]interface{}{
-				"parent": map[string]interface{}{
-					"child1": "value1",
-				},
-			},
-			want: map[string]interface{}{
-				"parent": map[string]interface{}{
-					"child1": "value1",
-					"child2": "value2",
-				},
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := setValue(tt.hierarchy, tt.value, tt.data)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("setValue() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-// TestSetValueInternal tests the setValueInternal function which recursively creates and maps nested dictionaries.
-func TestSetValueInternal(t *testing.T) {
-	tests := []struct {
-		name      string
-		hierarchy []string
-		value     any
-		inside    map[string]any
-		want      any
-	}{
-		{
-			name:      "single level",
-			hierarchy: []string{"key"},
-			value:     "value",
-			inside:    map[string]any{},
-			want:      "value",
-		},
-		{
-			name:      "two levels",
-			hierarchy: []string{"parent", "child"},
-			value:     "value",
-			inside:    map[string]any{},
-			want:      map[string]any{"child": "value"},
-		},
-		{
-			name:      "three levels",
-			hierarchy: []string{"level1", "level2", "level3"},
-			value:     "value",
-			inside:    map[string]any{},
-			want:      map[string]any{"level2": map[string]any{"level3": "value"}},
-		},
-		{
-			name:      "existing nested data",
-			hierarchy: []string{"parent", "child1", "grandchild"},
-			value:     "value",
-			inside: map[string]any{
-				"parent": map[string]any{
-					"child2": "existing",
-				},
-			},
-			want: map[string]any{
-				"child1": map[string]any{
-					"grandchild": "value",
-				},
-				"child2": "existing",
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := setValueInternal(tt.hierarchy, tt.value, tt.inside)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("setValueInternal() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 // TestMap tests the Map method with various configurations.
 func TestMap(t *testing.T) {
 	// Create a temporary mapping file
@@ -315,7 +199,7 @@ func TestMap(t *testing.T) {
 			},
 			expectedOutput: `{"property1":1,"property2":{"property3":"hello","property5":true},"property4":2.3}
 {"property1":2,"property2":{"property3":"world","property5":false},"property4":3.4}`,
-			wantErr:        false,
+			wantErr: false,
 		},
 		{
 			name: "yaml output",
